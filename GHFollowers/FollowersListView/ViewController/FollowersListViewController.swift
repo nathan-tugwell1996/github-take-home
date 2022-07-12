5d3a8d83993106af26904ab8ef6d8cc70877e7a4
Nathan Tugwell <nathan.tugwell@me.com>
2022-07-12 21:53:33 +0100

    refactored networking result type

--- GHFollowers/FollowersListView/ViewController/FollowersListViewController.swift
+++ GHFollowers/FollowersListView/ViewController/FollowersListViewController.swift
@@ -15,13 +15,13 @@ class FollowersListViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentNTAlertOnMainThread(title: "Error", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                    self.presentNTAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
             }
-            print(followers.count)
-            print(followers)
         }
     }
     
--- GHFollowers/NTError.swift
+++ GHFollowers/NTError.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-enum ErrorMessage: String {
+enum NTError: String, Error {
     case invalidUsername = "This username is incorrect, please try again."
     case unableToComplete = "Unable to complete request. Please check your internet"
     case invalidResponse = "Invalid Response from the server, Please try again."
