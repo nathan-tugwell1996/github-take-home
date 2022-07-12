728ee428952beb85723712e82ba8616fbc47de10
Nathan Tugwell <nathan.tugwell@me.com>
2022-07-12 23:24:41 +0100

    created custom collection view cell

---
+++ GHFollowers/FollowersListView/View/FollowerCell.swift
@@ -0,0 +1,46 @@
+//
+//  FollowerCell.swift
+//  GHFollowers
+//
+//  Created by Nathan Tugwell on 12/07/2022.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    let padding: CGFloat = 8
+    
+    let avatarImage = NTAvatarImageView(frame: .zero)
+    let usernameLabel = NTTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avatarImage)
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            avatarImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImage.heightAnchor.constraint(equalTo: avatarImage.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
---
+++ GHFollowers/UIExtension/NTAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  NTAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Nathan Tugwell on 12/07/2022.
+//
+
+import UIKit
+
+class NTAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
