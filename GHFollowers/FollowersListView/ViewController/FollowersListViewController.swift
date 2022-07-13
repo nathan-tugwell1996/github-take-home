baf6cea48cb75579b0fb3a1c8a85fb558cd6b0bf
Nathan Tugwell <nathan.tugwell@me.com>
2022-07-13 14:09:59 +0100

    refactored flow layout and added memory management

--- GHFollowers/FollowersListView/ViewController/FollowersListViewController.swift
+++ GHFollowers/FollowersListView/ViewController/FollowersListViewController.swift
@@ -38,32 +38,21 @@ class FollowersListViewController: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createFlowLayout())
+        let flow = CollectionViewLayoutHelper.createFlowLayout(in: view)
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flow)
         view.addSubview(collectionView)
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    func createFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        return flowLayout
-    }
-    
     func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
             switch result {
             case .success(let followers):
                 self.followers = followers
                 self.updateData()
             case .failure(let error):
-                    self.presentNTAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
+                self.presentNTAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
