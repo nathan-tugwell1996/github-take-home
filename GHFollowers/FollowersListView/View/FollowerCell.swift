c2efbbfbe5525762101f7b307b9b9bd6c0361299
Nathan Tugwell <nathan.tugwell@me.com>
2022-07-13 20:38:38 +0100

    added cache

--- GHFollowers/FollowersListView/View/FollowerCell.swift
+++ GHFollowers/FollowersListView/View/FollowerCell.swift
@@ -25,6 +25,7 @@ class FollowerCell: UICollectionViewCell {
     
     func set(follower: Follower) {
         usernameLabel.text = follower.login
+        avatarImage.downloadImage(urlString: follower.avatarUrl)
     }
     
     private func configure() {
